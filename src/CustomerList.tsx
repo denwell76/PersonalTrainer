8001f46bac4b359cea00948387718c9bd24fb06e
denwell76 <danielnieminen04@gmail.com>
2025-05-04 15:19:18 +0300

    csv deleted for now, due to it not working

--- src/CustomerList.tsx
+++ src/CustomerList.tsx
@@ -7,10 +7,7 @@ import AddCustomer from './components/AddCustomer';
 import EditCustomer from './components/EditCustomer';
 import AddTraining from './components/AddTraining';
 import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { saveAs } from 'file-saver';
-import { parse } from 'papaparse';
 
 
 ModuleRegistry.registerModules([AllCommunityModule]);
@@ -82,20 +79,6 @@ export default function CustomerList() {
     },
   ]);
 
-  const exportToCSV = () => {
-    const filteredData = customers.map(({ firstname, lastname, streetaddress, postcode, city, email, phone }) => ({
-      firstname,
-      lastname,
-      streetaddress,
-      postcode,
-      city,
-      email,
-      phone,
-    }));
-    const csv = parse(filteredData, { header: true }).data;
-    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
-    saveAs(blob, 'customers.csv');
-  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -104,9 +87,6 @@ export default function CustomerList() {
   return (
     <div>
       <h1>Customer List</h1>
-      <Button variant="contained" color="primary" onClick={exportToCSV}>
-        Export to CSV
-      </Button>
       <AddCustomer fetchCustomers={refreshCustomers} />
       <div className="ag-theme-alpine" style={{ height: 400, width: '100%' }}>
         <AgGridReact
